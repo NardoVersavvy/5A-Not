0f0638ff9a6b4560d00812cc720f2599cc86cd44
Nardi <nardidumessa@gmail.com>
2022-03-07 18:02:17 +0300

    update after review2

---
+++ dist/_nuxt/static/1646665269/power/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F5A\u002F_nuxt\u002Fstatic\u002F1646665269",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpower",config:{axios:{browserBaseURL:void 0},_app:{basePath:"\u002F5A\u002F",assetsPath:"\u002F5A\u002F_nuxt\u002F",cdnURL:a}}}}(null));
