3d1f40af4bb4de1406650186ce427fcfc1246978
Nardi <nardidumessa@gmail.com>
2022-03-15 11:04:41 +0300

    baseline update

---
+++ dist/_nuxt/static/1647331445/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcharity","\u002Femployee","\u002Fcontact","\u002Ftax","\u002Fpractice","\u002Fnews","\u002Fjournal","\u002Fpublication","\u002Fmining","\u002Fteam","\u002Fabout","\u002Fblog","\u002Ftelecom","\u002Fcoporate","\u002Finsight","\u002Fpower"]})
